08e89b6ed0ad5c8509ff023197b2b29bbdd85b0f
Quentin Krammer <bopatzable@gmail.com>
2024-09-13 11:00:00 +0200

    provide animations for primeNg to work

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // This is needed cause primeNgs official installation setup is not working since angular 17
+    // broken primeNg docs: https://primeng.org/installation#animations
+    // workaround: https://github.com/primefaces/primeng/issues/14843
+    provideAnimations(),
+  ],
 };
